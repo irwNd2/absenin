41e51703aa99c80353f412e79717fd903850dbc8
irwNd2 <irwandduadua@gmail.com>
2024-07-19 16:07:38 +0700

    fix: router.replace on login, create list of tabs for the layout

--- client-2/app/(inside)/student-list.tsx
+++ client-2/app/(inside)/student-list.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const InsidePage = () => {
+const StudentList = () => {
   return (
     <View>
-      <Text>Inside Page</Text>
+      <Text>StudentList</Text>
     </View>
   );
 };
 
-export default InsidePage;
+export default StudentList;
--- client-2/app/_layout.tsx
+++ client-2/app/_layout.tsx
@@ -1,7 +1,11 @@
+import "react-native-gesture-handler";
 import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const InitialLayout = () => {
   const { authState, initialized } = useAuth();
@@ -12,9 +16,10 @@ const InitialLayout = () => {
     if (!initialized) return;
 
     const inAuthGroup = segments[0] === "(inside)";
-    console.log(authState, "auth group");
     if (authState?.authenticated && !inAuthGroup) {
-      router.replace("/(inside)");
+      setTimeout(() => {
+        router.replace("/(inside)");
+      }, 100);
     } else if (!authState?.authenticated) {
       router.replace("/");
     }
@@ -22,17 +27,20 @@ const InitialLayout = () => {
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='(inside)' options={{ headerShown: false }} />
     </Stack>
   );
 };
 
 const RootLayout = () => {
   return (
-    <AuthProvider>
-      <GestureHandlerRootView>
-        <InitialLayout />
-      </GestureHandlerRootView>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <GestureHandlerRootView>
+          <InitialLayout />
+        </GestureHandlerRootView>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
