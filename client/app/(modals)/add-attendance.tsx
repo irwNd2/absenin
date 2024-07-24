a94418ca470916d85dff32c1be6bfc5986e095ed
irwNd2 <irwandduadua@gmail.com>
2024-07-24 16:36:34 +0700

    ui add attendance with dropdown select and datetimepicker

--- client/app/(modals)/add-attendance.tsx
+++ client/app/(modals)/add-attendance.tsx
@@ -1,68 +1,185 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 import { useAllSubject } from "@/hooks/useSubject";
 import { useAllClasses } from "@/hooks/useClasses";
-import { Picker } from "@react-native-picker/picker";
 import { useEffect, useState } from "react";
 import { Subject } from "@/types/Subject";
 import { Class } from "@/types/Class";
-import { DropdownSelectList } from "@/types/ComponentsProp";
-import { SelectList } from "react-native-dropdown-select-list";
 import Colors from "@/constants/Colors";
+import { Dropdown } from "react-native-element-dropdown";
+import RNDateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import { dateFormat, timeFormat } from "@/utils/time";
 
 const AddAttendance = () => {
-  const { data: subjectList, isLoading: subjectLoading } = useAllSubject();
-  const { data: classList, isLoading: classLoading } = useAllClasses();
-  const [subjectData, setSubjectData] = useState<DropdownSelectList[] | null>(
-    null
-  );
-  const [classData, setClassData] = useState<DropdownSelectList[] | null>(null);
+  const { data: subjectList } = useAllSubject();
+  const { data: classList } = useAllClasses();
+  const [subjectData, setSubjectData] = useState<Subject[] | null>(null);
+  const [classData, setClassData] = useState<Class[] | null>(null);
 
   useEffect(() => {
-    if (subjectList) {
-      const newSubject: DropdownSelectList[] = subjectList.map(
-        (subject: Subject) => ({
-          key: subject.id,
-          value: subject.name,
-        })
-      );
-      setSubjectData(newSubject);
-    }
-    if (classList) {
-      const newClass: DropdownSelectList[] = classList.map((cls: Class) => ({
-        key: cls.id,
-        value: cls.name,
-      }));
-      setClassData(newClass);
-    }
+    if (subjectList) setSubjectData(subjectList);
+    if (classList) setClassData(classList);
   }, [subjectList, classList]);
 
   const [selectedSubjectID, setSelectedSubjectID] = useState<number | null>(
     null
   );
   const [selectedClassID, setSelectedClassID] = useState<number | null>(null);
+  const [date, setDate] = useState<Date>(new Date());
+  const [time, setTime] = useState<Date>(new Date());
+  const [showDateAndroid, setShowDateAndroid] = useState(false);
+  const [showTimeAndroid, setShowTimeAndroid] = useState(false);
 
-  //   useEffect(() => {
-  //     console.log(selectedSubjectID);
-  //   }, [selectedSubjectID]);
+  const [isFocus, setIsFocus] = useState(false);
+  const [isClassFocus, setIsClassFocus] = useState(false);
+
+  useEffect(() => {
+    console.log(time);
+  }, [time]);
+
+  const onDateChange = (
+    { type }: DateTimePickerEvent,
+    selectedDate: Date | undefined
+  ) => {
+    if (type === "set") {
+      if (selectedDate) setDate(selectedDate);
+      if (showDateAndroid) setShowDateAndroid(!showDateAndroid);
+    } else {
+      setShowDateAndroid(!showDateAndroid);
+    }
+  };
+
+  const onTimeChange = (
+    { type }: DateTimePickerEvent,
+    selectedTime: Date | undefined
+  ) => {
+    if (type === "set") {
+      if (selectedTime) setTime(selectedTime);
+      if (showTimeAndroid) setShowTimeAndroid(!showTimeAndroid);
+    } else {
+      setShowTimeAndroid(!showTimeAndroid);
+    }
+  };
+
   if (subjectData && classData) {
     return (
       <View style={styles.container}>
         <View>
           <Text style={styles.title}>Pilih Mata Pelajaran</Text>
-          <SelectList
+          <Dropdown
+            style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            iconStyle={styles.iconStyle}
             data={subjectData}
-            setSelected={(val: number) => setSelectedSubjectID(val)}
-            save='key'
+            maxHeight={300}
+            labelField='name'
+            valueField='id'
+            placeholder={!isFocus ? "Pilih dari daftar berikut" : "..."}
+            searchPlaceholder='Search...'
+            onFocus={() => setIsFocus(true)}
+            onBlur={() => setIsFocus(false)}
+            onChange={(item: Subject) => {
+              setSelectedSubjectID(item.id);
+              setIsFocus(false);
+            }}
           />
         </View>
         <View>
           <Text style={styles.title}>Pilih Kelas</Text>
-          <SelectList
+          <Dropdown
+            style={[styles.dropdown, isClassFocus && { borderColor: "blue" }]}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            iconStyle={styles.iconStyle}
             data={classData}
-            setSelected={(val: number) => setSelectedSubjectID(val)}
-            save='key'
+            search
+            maxHeight={300}
+            labelField='name'
+            valueField='id'
+            placeholder={!isClassFocus ? "Pilih dari daftar berikut" : "..."}
+            searchPlaceholder='Search...'
+            onFocus={() => setIsClassFocus(true)}
+            onBlur={() => setIsClassFocus(false)}
+            onChange={(item: Class) => {
+              setSelectedClassID(item.id);
+              setIsClassFocus(false);
+            }}
           />
         </View>
+        <Text style={styles.title}>Pilih Tanggal</Text>
+        {Platform.OS === "ios" && (
+          <View style={{ flexDirection: "row" }}>
+            <RNDateTimePicker
+              minimumDate={new Date()}
+              value={date}
+              onChange={onDateChange}
+            />
+            <RNDateTimePicker
+              minimumDate={new Date()}
+              value={date}
+              mode='time'
+              onChange={onTimeChange}
+            />
+          </View>
+        )}
+        {Platform.OS === "android" && (
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <TouchableOpacity
+              style={{
+                height: 40,
+                padding: 10,
+                borderRadius: 8,
+                backgroundColor: "grey",
+              }}
+              onPress={() => setShowDateAndroid(true)}
+            >
+              <Text style={{ color: "white", fontSize: 15, fontWeight: 900 }}>
+                {dateFormat(date)}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                height: 40,
+                padding: 10,
+                borderRadius: 8,
+                backgroundColor: "grey",
+              }}
+              onPress={() => setShowTimeAndroid(true)}
+            >
+              <Text style={{ color: "white", fontSize: 15, fontWeight: 900 }}>
+                {timeFormat(time)}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+
+        {Platform.OS === "android" && showDateAndroid && (
+          <>
+            <RNDateTimePicker
+              minimumDate={new Date()}
+              value={date}
+              onChange={onDateChange}
+            />
+          </>
+        )}
+        {Platform.OS === "android" && showTimeAndroid && (
+          <>
+            <RNDateTimePicker
+              minimumDate={new Date()}
+              value={date}
+              mode='time'
+              onChange={onTimeChange}
+            />
+          </>
+        )}
         <TouchableOpacity style={styles.buttonConfirm}>
           <Text style={styles.btnText}>Tambah</Text>
         </TouchableOpacity>
@@ -98,6 +215,40 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 20,
     color: "white",
+  },
+
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
   },
 });
 
---
+++ client/utils/time.ts
@@ -0,0 +1,36 @@
+export const dateFormat = (date: Date) => {
+  return date.toLocaleDateString("id-ID", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+};
+
+export const timeFormat = (date: Date) => {
+  return date.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+};
+
+export const combineDateTime = (date: Date, time: Date) => {
+  // Extract date components
+  const year = date.getUTCFullYear();
+  const month = date.getUTCMonth(); // Note: months are 0-indexed
+  const day = date.getUTCDate();
+
+  // Extract time components
+  const hours = time.getUTCHours();
+  const minutes = time.getUTCMinutes();
+  const seconds = time.getUTCSeconds();
+  const milliseconds = time.getUTCMilliseconds();
+
+  // Create a new Date object combining the date and time components
+  const combinedDate = new Date(
+    Date.UTC(year, month, day, hours, minutes, seconds, milliseconds)
+  );
+
+  // Return the combined date as an ISO string
+  return combinedDate.toISOString();
+};
